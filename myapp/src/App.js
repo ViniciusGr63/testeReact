9563af154f0f4d64375f06d142deca30c6f806ba
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2024-02-22 20:45:42 +0000

    new component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Frase from './components/Frase';
+import Logo from './components/Logo';
+import Ola  from './components/Ola';
 
 function App() {
+  const nome = "Logo React";
+  const ft = 'https://www.iteachrecruiters.com/img/blog/logo/react-native.png';
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
         <Frase/>
+        <Logo/>
+        
       </header>
+      <body>
+
+      <Ola 
+      foto = {ft}
+      title={nome} />
+       
+      
+      </body>
     </div>
   );
 }
